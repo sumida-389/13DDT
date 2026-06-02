e2814873dbfb067ecf10558c3ba335c988a7aa69
Sumida Gande <sumidagande@gmail.com>
2026-06-02 12:01:49 +1200

    I redid the code and forced the database in a specific file so I opened the correct file.

--- studyapp/main.py
+++ studyapp/main.py
@@ -1,12 +1,54 @@
+import tkinter as tk
+from tkinter import *
 import sqlite3
-import hashlib  
+import os
+from datetime import datetime
 
-def password(user_password):
-    return hashlib.sha256(user_password.encode()).hexdigest() # Turns a plaintext password into a secure hash
+def create_tables():
+    # Force DB to always be in the same folder as this file
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(BASE_DIR, "studyapp.db")
 
-user_password=input("Enter a password: ")
-print(password(user_password))
+    connection = sqlite3.connect(db_path)
+    cursor = connection.cursor()
 
-def create_database():
-    connection = sqlite3.connect("studyapp.db") # Create/Connects the database file
-    cursor = connection.cursor() # Create a cursor object to interact with the database
+    print("DB LOCATION:", db_path)
+
+    #users table
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL,
+            ai_features INTEGER NOT NULL DEFAULT 1,
+        )
+    """)
+
+    #notes table
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS notes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL,
+            title TEXT NOT NULL,
+            body TEXT NOT NULL DEFAULT '',
+            summary TEXT DEFAULT NULL,
+            FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+        )
+    """)
+
+    #deck table
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS decks (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL,
+            name TEXT NOT NULL,
+            created_at TEXT NOT NULL,
+            FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+        )
+    """)
+
+    connection.commit()
+    connection.close()
+
+
+create_tables()
